1dc7b37aafa6bb5100587ae4f1397cda30e056c9
Amy Willis <amy.willis7@gmail.com>
2017-10-23 13:44:42 -0700

    package structure

---
+++ test.R
@@ -0,0 +1,33 @@
+### Amy Willis, October 2017
+
+## A script to enable compiling, building and testing the CatchAll R package
+
+## Create and set the working directory
+directory <- "/Users/adwillis/software/CatchAll" ## your local copy here
+setwd(directory)
+
+# Download some required packages
+require(devtools)
+require(roxygen2)
+require(testthat)
+require(knitr)
+require(rstudioapi)
+require(Rd2roxygen)
+devtools::install_github("hadley/pkgdown")
+library(pkgdown)
+
+# 
+create_package("CatchAll")
+build()
+install()
+test_data_set_1 <- read.csv("data/10000_geo_.5_2.csv", header = F)
+x <- CatchAll(test_data_set_1)
+x[19:25, 1:5]
+x[19:25, 12:14]
+
+## To build the full site -- not yet!!
+# roxygenise()
+# roxygen_and_build(directory)
+# install()
+# build_site()
+# check()
