4c75d3afde3d5046cdd4fa1f7eb1e26d7e707c2a
Teresa Zhan <t.zhan117@gmail.com>
2017-12-31 11:50:01 -0800

    SEFourMixed

--- R/SEFourMixedExpA1.R
+++ R/SEFourMixedExpA1.R
@@ -1599,4 +1599,6 @@ SEFourMixedExpA1 <- function(t1, t2, t3, t4, t5, t6, t7, a, sHatSubset) {
     return(list("flag"=1))
     break
   }
+  
+  a
 }
--- R/SEFourMixedExpA2.R
+++ R/SEFourMixedExpA2.R
@@ -1633,5 +1633,6 @@ SEFourMixedExpA2 <- function(t1, t2, t3, t4, t5, t6, t7, a, sHatSubset) {
     break
   }
 #}
+  a
 
 }
